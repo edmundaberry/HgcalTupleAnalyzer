1336b4a70caa2415b3cc7b7d7a0a82e8e2310e45
Edmund <edmund.a.berry@cern.ch>
2014-09-01 13:20:44 +0200

    Still developing...

--- macros/analysisClass.C
+++ macros/analysisClass.C
@@ -1,13 +1,61 @@
 #include "analysisClass.h"
-#include "GenJet.h"
+#include "PFPrunedR5Jet.h"
 #include "GenParticle.h"
 #include "Collection.h"
 #include "HgcalTupleTree.h"
 
 #include "TH1F.h"
 
+void preparePFCandAxis( TH1F * hist ) {
+  hist -> GetXaxis() -> SetBinLabel(1, "Undefined");
+  hist -> GetXaxis() -> SetBinLabel(2, "Charged had");
+  hist -> GetXaxis() -> SetBinLabel(3, "Electron");
+  hist -> GetXaxis() -> SetBinLabel(4, "Muon");
+  hist -> GetXaxis() -> SetBinLabel(5, "Photon");
+  hist -> GetXaxis() -> SetBinLabel(6, "Neutral had");
+  hist -> GetXaxis() -> SetBinLabel(7, "HF had");
+  hist -> GetXaxis() -> SetBinLabel(8, "HF egamma");
+  hist -> GetXaxis() -> SetBinLabel(9, "All");
+}
+
+void preparePFCandAxis( TH2F * hist ) {
+  hist -> GetXaxis() -> SetBinLabel(1, "Undefined");
+  hist -> GetXaxis() -> SetBinLabel(2, "Charged had");
+  hist -> GetXaxis() -> SetBinLabel(3, "Electron");
+  hist -> GetXaxis() -> SetBinLabel(4, "Muon");
+  hist -> GetXaxis() -> SetBinLabel(5, "Photon");
+  hist -> GetXaxis() -> SetBinLabel(6, "Neutral had");
+  hist -> GetXaxis() -> SetBinLabel(7, "HF had");
+  hist -> GetXaxis() -> SetBinLabel(8, "HF egamma");
+  hist -> GetXaxis() -> SetBinLabel(9, "All");
+}
+
+template <class Jet>
+void fillPFCandVsCount ( Jet & jet, TH2F* hist ){
+  
+  const int n_ids = 8;
+  int pfcand_ids[n_ids + 1];
+  memset ( pfcand_ids, 0, sizeof (pfcand_ids));
+  
+  int nPFCands = jet.getNPFCandidates();
+  for (int iPFCand = 0; iPFCand < nPFCands; ++iPFCand){
+    PFCand cand = jet.getPFCandidate( iPFCand );
+    pfcand_ids[cand.Id()]++;
+    pfcand_ids[n_ids]++;
+  }
+  for (int i = 0; i <= n_ids; ++i) hist -> Fill(i, pfcand_ids[i]);
+
+}
+
 void analysisClass::loop(){
 
+  //--------------------------------------------------------------------------------
+  // Store important variables
+  //--------------------------------------------------------------------------------
+
+  float HGCAL_abseta_min = 1.4;
+  float HGCAL_abseta_max = 3.0;
+  
   //--------------------------------------------------------------------------------
   // Declare tree(s)
   //--------------------------------------------------------------------------------
@@ -19,9 +67,17 @@ void analysisClass::loop(){
   // Make histograms
   //--------------------------------------------------------------------------------
 
-  TH1F * h_DR_genJet_vbfQuark     = makeTH1F("DR_genJet_vbfQuark"    , 100, 0, 5.0);
-  TH1F * h_PtPull_genJet_vbfQuark = makeTH1F("PtPull_genJet_vbfQuark", 100, -1.0, 1.0);
-  TH2F * h_PtPull_vs_DR_genJet_vbfQuark = makeTH2F( "PtPull_vs_DR_genJet_vbfQuark", 100, -1.0, 1.0, 100, 0, 5.0);
+  TH1F * h_DR_pfJet_vbfQuark = makeTH1F("DR_pfJet_vbfQuark", 100, 0, 5.0);
+  TH1F * h_nsubj_quarkJets   = makeTH1F("nsubj_quarkJets"   , 100, 0, 1);
+  TH1F * h_nsubj_gluonJets   = makeTH1F("nsubj_gluonJets"   , 100, 0, 1);
+
+  TH2F * h_pfCandId_vs_count_vbfJets = makeTH2F("pfCandId_vs_count_vbfJets", 9, -0.5, 8.5, 201, -0.5, 200.5);
+  TH2F * h_pfCandId_vs_count_hbJets  = makeTH2F("pfCandId_vs_count_hbJets" , 9, -0.5, 8.5, 201, -0.5, 200.5);
+  TH2F * h_pfCandId_vs_count_hgcJets = makeTH2F("pfCandId_vs_count_hgcJets", 9, -0.5, 8.5, 201, -0.5, 200.5);
+			
+  preparePFCandAxis(h_pfCandId_vs_count_vbfJets);
+  preparePFCandAxis(h_pfCandId_vs_count_hbJets );
+  preparePFCandAxis(h_pfCandId_vs_count_hgcJets);
   
   //--------------------------------------------------------------------------------
   // Loop over the events
@@ -42,45 +98,35 @@ void analysisClass::loop(){
     tree -> GetEntry (iEvent);
 
     //--------------------------------------------------------------------------------
-    // Build initial inclusive collections
+    // GEN particle collections
     //--------------------------------------------------------------------------------
     
-    CollectionPtr c_gen_all   ( new Collection(*tree, tree -> GenParticlePt -> size()));
-    CollectionPtr c_genJet_all( new Collection(*tree, tree -> GenJetPt      -> size()));
+    // All GEN particles
+    CollectionPtr c_gen_all  ( new Collection(*tree, tree -> GenParticlePt -> size()));
     
-    //--------------------------------------------------------------------------------
-    // GEN particle sorting
-    //--------------------------------------------------------------------------------
-    
-    CollectionPtr c_gen_VBFQuarks = c_gen_all -> SkimByID<GenParticle>(GEN_PARTICLE_IS_HARD_SCATTER_VBF_QUARK);
-    CollectionPtr c_gen_gluons    = c_gen_all -> SkimByID<GenParticle>(GEN_PARTICLE_IS_HARD_SCATTER_GLUON);
-    
-    int n_VBF_quarks = c_gen_VBFQuarks -> GetSize();
-    int n_gen_jets   = c_genJet_all -> GetSize();
+    // VBF quarks
+    CollectionPtr c_gen_VBF     = c_gen_all -> SkimByID<GenParticle>(GEN_PARTICLE_IS_HARD_SCATTER_VBF_QUARK);
+    CollectionPtr c_gen_VBF_fid = c_gen_VBF -> SkimByAbsEtaRange<GenParticle> ( HGCAL_abseta_min, HGCAL_abseta_max );
 
-    // Make matching plots
+    // Gluons
+    CollectionPtr c_gen_glu     = c_gen_all -> SkimByID<GenParticle>(GEN_PARTICLE_IS_HARD_SCATTER_GLUON);
+    CollectionPtr c_gen_glu_fid = c_gen_glu -> SkimByAbsEtaRange <GenParticle> ( HGCAL_abseta_min, HGCAL_abseta_max );
 
-    for (int i_VBF_quark = 0; i_VBF_quark < n_VBF_quarks; ++i_VBF_quark){
-      GenParticle vbf_quark = c_gen_VBFQuarks -> GetConstituent<GenParticle>(i_VBF_quark);
-      for (int i_gen_jet = 0; i_gen_jet < n_gen_jets; ++i_gen_jet){
-	GenJet gen_jet = c_genJet_all -> GetConstituent<GenJet>(i_gen_jet);
-	double deltaR  = vbf_quark.DeltaR (&gen_jet);
-	double pt_pull = ( gen_jet.Pt() - vbf_quark.Pt() ) / ( vbf_quark.Pt() );
-	
-	h_DR_genJet_vbfQuark           -> Fill ( deltaR  );
-	h_PtPull_genJet_vbfQuark       -> Fill ( pt_pull );
-	h_PtPull_vs_DR_genJet_vbfQuark -> Fill ( pt_pull, deltaR) ;
-      }
-    }
-    
-    CollectionPtr c_genJet_VBF = c_genJet_all -> SkimByRequireDRMatch<GenJet, GenParticle>( c_gen_VBFQuarks, 0.2 );
-    
     //--------------------------------------------------------------------------------
-    // Examine
+    // PFJet collections
     //--------------------------------------------------------------------------------
     
-    c_gen_VBFQuarks -> examine<GenParticle> ( "VBF quarks" );
-    c_genJet_VBF    -> examine<GenJet>      ( "VBF GenJets" );
+    // All pruned PF jets
+    CollectionPtr c_pfjet_prunedR5_all ( new Collection(*tree, tree -> PFCA4PrunedJetRCut5Pt -> size()));
+    
+    // VBF jets
+    CollectionPtr c_pfjet_prunedR5_VBF     = c_pfjet_prunedR5_all -> SkimByRequireDRMatch<PFPrunedR5Jet, GenParticle>(c_gen_VBF, 0.4);
+    CollectionPtr c_pfjet_prunedR5_VBF_fid = c_pfjet_prunedR5_VBF -> SkimByAbsEtaRange<PFPrunedR5Jet> ( HGCAL_abseta_min, HGCAL_abseta_max ); 
+    
+    // Gluon jets
+    CollectionPtr c_pfjet_prunedR5_glu     = c_pfjet_prunedR5_all -> SkimByRequireDRMatch<PFPrunedR5Jet, GenParticle>(c_gen_glu, 0.4);
+    CollectionPtr c_pfjet_prunedR5_glu_fid = c_pfjet_prunedR5_glu -> SkimByAbsEtaRange<PFPrunedR5Jet> ( HGCAL_abseta_min, HGCAL_abseta_max ); 
+    
     
   }
 
