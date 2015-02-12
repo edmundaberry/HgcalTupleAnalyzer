ca12f7b0e6cc8a19daaad90c39b592ae73af1284
Edmund A Berry <Edmund.A.Berry@CERN.CH>
2015-02-12 16:23:51 +0100

    All RecHit types can come from all PFClusters

--- src/HGCHEBCluster.C
+++ src/HGCHEBCluster.C
@@ -1,6 +1,8 @@
 #include "HGCHEBCluster.h"
+#include "HGCEERecHit.h"
 #include "Object.h"
 #include "IDTypes.h"
+#include "RecHitTypes.h"
 
 #include <cmath>
 
@@ -8,11 +10,13 @@
 
 HGCHEBCluster::HGCHEBCluster():
   PFCluster(),
-  m_et (-1.0) {}
+  m_et (-1.0)
+{}
 
-HGCHEBCluster::HGCHEBCluster(Collection& c, unsigned short i, short j ):
+HGCHEBCluster::HGCHEBCluster(Collection& c, unsigned int i, int j ):
   PFCluster(c,i),
-  m_et (-1.0) {}
+  m_et (-1.0)
+{}
 
 // Kinematic variables
 
@@ -20,7 +24,7 @@ double & HGCHEBCluster::Pt        () { Et(); return m_et; }
 double & HGCHEBCluster::Eta       () { return m_collection -> GetData() -> HGCHEBPFClusterEta        -> at ( m_raw_index ); } 
 double & HGCHEBCluster::Phi       () { return m_collection -> GetData() -> HGCHEBPFClusterPhi        -> at ( m_raw_index ); } 
 double & HGCHEBCluster::Energy    () { return m_collection -> GetData() -> HGCHEBPFClusterEnergy     -> at ( m_raw_index ); } 
-		        							       
+
 double & HGCHEBCluster::X         () { return m_collection -> GetData() -> HGCHEBPFClusterX          -> at ( m_raw_index ); } 
 double & HGCHEBCluster::Y         () { return m_collection -> GetData() -> HGCHEBPFClusterY          -> at ( m_raw_index ); } 
 double & HGCHEBCluster::Z         () { return m_collection -> GetData() -> HGCHEBPFClusterZ          -> at ( m_raw_index ); } 
@@ -35,13 +39,46 @@ std::ostream& operator<<(std::ostream& stream, HGCHEBCluster & object) {
   return stream;
 }
 
-HGCHEBRecHit HGCHEBCluster::getHGCHEBRecHit (int i){
-  int rechit_index = m_collection -> GetData() -> HGCHEBPFClusterRecHitIndex -> at ( m_raw_index )[i];
-  HGCHEBRecHit retval ( *m_collection, rechit_index );
+HGCEERecHit HGCHEBCluster::getHGCEERecHit (int i){
+  int rechit_index = m_collection -> GetData() -> HGCHEBPFClusterHGCEERecHitIndex  -> at ( m_raw_index )[i];
+  HGCEERecHit retval ( HGCHEB, *m_collection, rechit_index );
   retval.SetClusterEta( Eta() );
+  retval.SetClusterX  ( X()   );
+  retval.SetClusterY  ( Y()   );
+  retval.SetClusterZ  ( Z()   );
   return retval;
 }
 
+
+HGCHEBRecHit HGCHEBCluster::getHGCHEBRecHit (int i){
+  int rechit_index = m_collection -> GetData() -> HGCHEBPFClusterHGCHEBRecHitIndex -> at ( m_raw_index )[i];
+  HGCHEBRecHit retval ( HGCHEB, *m_collection, rechit_index );
+  retval.SetClusterEta( Eta() );
+  retval.SetClusterX  ( X()   );
+  retval.SetClusterY  ( Y()   );
+  retval.SetClusterZ  ( Z()   );
+  return retval;
+}
+
+HGCHEFRecHit HGCHEBCluster::getHGCHEFRecHit (int i){
+  int rechit_index = m_collection -> GetData() -> HGCHEBPFClusterHGCHEFRecHitIndex -> at ( m_raw_index )[i];
+  HGCHEFRecHit retval ( HGCHEB, *m_collection, rechit_index );
+  retval.SetClusterEta( Eta() );
+  retval.SetClusterX  ( X()   );
+  retval.SetClusterY  ( Y()   );
+  retval.SetClusterZ  ( Z()   );
+  return retval;
+}
+
+
+int HGCHEBCluster::getNHGCEERecHits (){
+  return m_collection -> GetData() -> HGCHEBPFClusterHGCEERecHitIndex -> at (m_raw_index).size();
+}
+
 int HGCHEBCluster::getNHGCHEBRecHits (){
-  return m_collection -> GetData() -> HGCHEBPFClusterRecHitIndex -> at (m_raw_index).size();
+  return m_collection -> GetData() -> HGCHEBPFClusterHGCHEBRecHitIndex -> at (m_raw_index).size();
+}
+
+int HGCHEBCluster::getNHGCHEFRecHits (){
+  return m_collection -> GetData() -> HGCHEBPFClusterHGCHEFRecHitIndex -> at (m_raw_index).size();
 }
--- src/HGCHEFCluster.C
+++ src/HGCHEFCluster.C
@@ -1,6 +1,8 @@
 #include "HGCHEFCluster.h"
+#include "HGCEERecHit.h"
 #include "Object.h"
 #include "IDTypes.h"
+#include "RecHitTypes.h"
 
 #include <cmath>
 
@@ -8,11 +10,13 @@
 
 HGCHEFCluster::HGCHEFCluster():
   PFCluster(),
-  m_et (-1.0) {}
+  m_et (-1.0)
+{}
 
-HGCHEFCluster::HGCHEFCluster(Collection& c, unsigned short i, short j ):
+HGCHEFCluster::HGCHEFCluster(Collection& c, unsigned int i, int j ):
   PFCluster(c,i),
-  m_et (-1.0) {}
+  m_et (-1.0)
+{}
 
 // Kinematic variables
 
@@ -20,7 +24,7 @@ double & HGCHEFCluster::Pt        () { Et(); return m_et; }
 double & HGCHEFCluster::Eta       () { return m_collection -> GetData() -> HGCHEFPFClusterEta        -> at ( m_raw_index ); } 
 double & HGCHEFCluster::Phi       () { return m_collection -> GetData() -> HGCHEFPFClusterPhi        -> at ( m_raw_index ); } 
 double & HGCHEFCluster::Energy    () { return m_collection -> GetData() -> HGCHEFPFClusterEnergy     -> at ( m_raw_index ); } 
-		        							       
+
 double & HGCHEFCluster::X         () { return m_collection -> GetData() -> HGCHEFPFClusterX          -> at ( m_raw_index ); } 
 double & HGCHEFCluster::Y         () { return m_collection -> GetData() -> HGCHEFPFClusterY          -> at ( m_raw_index ); } 
 double & HGCHEFCluster::Z         () { return m_collection -> GetData() -> HGCHEFPFClusterZ          -> at ( m_raw_index ); } 
@@ -35,13 +39,46 @@ std::ostream& operator<<(std::ostream& stream, HGCHEFCluster & object) {
   return stream;
 }
 
-HGCHEFRecHit HGCHEFCluster::getHGCHEFRecHit (int i){
-  int rechit_index = m_collection -> GetData() -> HGCHEFPFClusterRecHitIndex -> at ( m_raw_index )[i];
-  HGCHEFRecHit retval ( *m_collection, rechit_index );
+HGCEERecHit HGCHEFCluster::getHGCEERecHit (int i){
+  int rechit_index = m_collection -> GetData() -> HGCHEFPFClusterHGCEERecHitIndex  -> at ( m_raw_index )[i];
+  HGCEERecHit retval ( HGCHEF, *m_collection, rechit_index );
   retval.SetClusterEta( Eta() );
+  retval.SetClusterX  ( X()   );
+  retval.SetClusterY  ( Y()   );
+  retval.SetClusterZ  ( Z()   );
   return retval;
 }
 
+
+HGCHEBRecHit HGCHEFCluster::getHGCHEBRecHit (int i){
+  int rechit_index = m_collection -> GetData() -> HGCHEFPFClusterHGCHEBRecHitIndex -> at ( m_raw_index )[i];
+  HGCHEBRecHit retval ( HGCHEF, *m_collection, rechit_index );
+  retval.SetClusterEta( Eta() );
+  retval.SetClusterX  ( X()   );
+  retval.SetClusterY  ( Y()   );
+  retval.SetClusterZ  ( Z()   );
+  return retval;
+}
+
+HGCHEFRecHit HGCHEFCluster::getHGCHEFRecHit (int i){
+  int rechit_index = m_collection -> GetData() -> HGCHEFPFClusterHGCHEFRecHitIndex -> at ( m_raw_index )[i];
+  HGCHEFRecHit retval ( HGCHEF, *m_collection, rechit_index );
+  retval.SetClusterEta( Eta() );
+  retval.SetClusterX  ( X()   );
+  retval.SetClusterY  ( Y()   );
+  retval.SetClusterZ  ( Z()   );
+  return retval;
+}
+
+
+int HGCHEFCluster::getNHGCEERecHits (){
+  return m_collection -> GetData() -> HGCHEFPFClusterHGCEERecHitIndex -> at (m_raw_index).size();
+}
+
+int HGCHEFCluster::getNHGCHEBRecHits (){
+  return m_collection -> GetData() -> HGCHEFPFClusterHGCHEBRecHitIndex -> at (m_raw_index).size();
+}
+
 int HGCHEFCluster::getNHGCHEFRecHits (){
-  return m_collection -> GetData() -> HGCHEFPFClusterRecHitIndex -> at(m_raw_index).size();
+  return m_collection -> GetData() -> HGCHEFPFClusterHGCHEFRecHitIndex -> at (m_raw_index).size();
 }
